a93598f310bbe6301a2a8c3765ce794c00c6377e
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-09-08 12:03:00 +0200

    third commit

--- rawg-frontend/src/main.tsx
+++ rawg-frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
