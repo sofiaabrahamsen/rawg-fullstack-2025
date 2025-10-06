504053446d3711c9f58c21c5819f19d3898dee7b
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-10-06 10:05:05 +0200

    added query.sql

---
+++ rawg-backend/queries/query.sql
@@ -0,0 +1,28 @@
+query:
+SELECT `game`.`id` AS `game_id`,
+  `game`.`name` AS `game_name`,
+  `game`.`metacritic` AS `game_metacritic`,
+  `game`.`background_image` AS `game_background_image`,
+  `genres`.`id` AS `genres_id`,
+  `genres`.`name` AS `genres_name`,
+  `genres`.`image_background` AS `genres_image_background`,
+  `stores`.`id` AS `stores_id`,
+  `stores`.`name` AS `stores_name`,
+  `stores`.`image_background` AS `stores_image_background`,
+  `parent_platforms`.`id` AS `parent_platforms_id`,
+  `parent_platforms`.`name` AS `parent_platforms_name`,
+  `parent_platforms`.`slug` AS `parent_platforms_slug`
+FROM `games` `game`
+  LEFT JOIN `games_has_genres` `game_genres` ON `game_genres`.`games_id` = `game`.`id`
+  LEFT JOIN `genres` `genres` ON `genres`.`id` = `game_genres`.`genres_id`
+  LEFT JOIN `games_has_stores` `game_stores` ON `game_stores`.`games_id` = `game`.`id`
+  LEFT JOIN `stores` `stores` ON `stores`.`id` = `game_stores`.`stores_id`
+  LEFT JOIN `games_has_parent_platforms` `game_parent_platforms` ON `game_parent_platforms`.`games_id` = `game`.`id`
+  LEFT JOIN `parent_platforms` `parent_platforms` ON `parent_platforms`.`id` = `game_parent_platforms`.`parent_platforms_id`
+WHERE `game`.`id` IN (
+    SELECT `game`.`id` AS `game_id`
+    FROM `games` `game`
+      LEFT JOIN `games_has_genres` `game_genres` ON `game_genres`.`games_id` = `game`.`id`
+      LEFT JOIN `genres` `genres` ON `genres`.`id` = `game_genres`.`genres_id`
+    WHERE `genres`.`id` = ?
+  ) -- PARAMETERS: [1]
--- rawg-backend/routes/gameRouter.ts
+++ rawg-backend/routes/gameRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SelectQueryBuilder } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Game } from "../entities/Game";
 import { Genre } from "../entities/Genre";
@@ -24,7 +25,84 @@ interface Response {
 const gameRouter = Router();
 const gameRepository = AppDataSource.getRepository(Game);
 
+const addGenreFilter = (
+  queryBuilder: SelectQueryBuilder<Game>,
+  genreId: number | undefined
+) => {
+  if (genreId) {
+    queryBuilder
+      .andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select("game.id")
+          .from(Game, "game")
+          .leftJoin("game.genres", "genres")
+          .where("genres.id = :genreId")
+          .getQuery();
+        return "game.id IN " + subQuery;
+      })
+      .setParameter("genreId", genreId);
+  }
+};
+
+const addGenrefilterNativeQuery = (
+  queryBuilder: SelectQueryBuilder<Game>,
+  genreId: number | undefined
+) => {
+  if (genreId) {
+    queryBuilder.andWhere(
+      "game.id IN (SELECT games_id FROM games_has_genres WHERE genres_id = :genreId)",
+      { genreId }
+    );
+  }
+};
+
+const addStoreFilter = (
+  queryBuilder: SelectQueryBuilder<Game>,
+  storeId: number | undefined
+) => {
+  if (storeId) {
+    queryBuilder
+      .andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select("game.id")
+          .from(Game, "game")
+          .leftJoin("game.stores", "stores")
+          .where("stores.id = :storeId")
+          .getQuery();
+        return "game.id IN " + subQuery;
+      })
+      .setParameter("storeId", storeId);
+  }
+};
+
+const addParentPlatformFilter = (
+  queryBuilder: SelectQueryBuilder<Game>,
+  parentPlatformId: number | undefined
+) => {
+  if (parentPlatformId) {
+    queryBuilder
+      .andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select("game.id")
+          .from(Game, "game")
+          .leftJoin("game.parent_platforms", "parent_platforms")
+          .where("parent_platforms.id = :parentPlatformId")
+          .getQuery();
+        return "game.id IN " + subQuery;
+      })
+      .setParameter("parentPlatformId", parentPlatformId);
+  }
+};
+
 gameRouter.get("/", async (req, res) => {
+  const genreId = req.query.genres ? Number(req.query.genres) : undefined;
+  const storeId = req.query.stores ? Number(req.query.stores) : undefined;
+  const parentPlatformId = req.query.platforms
+    ? Number(req.query.platforms)
+    : undefined;
   try {
     // Using QueryBuilder to fetch games along with their relations - genres, stores, and parent platforms
     const queryBuilder = gameRepository
@@ -32,6 +110,10 @@ gameRouter.get("/", async (req, res) => {
       .leftJoinAndSelect("game.genres", "genres")
       .leftJoinAndSelect("game.stores", "stores")
       .leftJoinAndSelect("game.parent_platforms", "parent_platforms");
+
+    addGenreFilter(queryBuilder, genreId);
+    addStoreFilter(queryBuilder, storeId);
+    addParentPlatformFilter(queryBuilder, parentPlatformId);
 
     const games = await queryBuilder.getMany();
 
